4c73a309c7046473569050fde36812ebf504a446
RStempa <stemparobin@gmail.com>
2023-02-08 16:46:09 +0100

    instantiate hero from main

--- RPGHeroes/Program.cs
+++ RPGHeroes/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace RPGHeroes
+﻿using RPGHeroes.Heroes;
+
+namespace RPGHeroes
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Mage mage = new Mage("Albert");
+            Console.WriteLine(mage.Name);
+            Console.WriteLine(mage.LevelAttributes.Strength);
+            //Console.WriteLine(mage.Equipment.ToArray());
+
         }
     }
 }
